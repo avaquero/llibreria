50549908e5b9ddc8c4f1dbd30dd69484d1c7c854
juli <juli.fer14@gmail.com>
2013-12-02 19:34:01 +0100

    Proves de realització de vistes

--- llibreria/urls.py
+++ llibreria/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from llibreria import views
 from django.contrib import admin
 admin.autodiscover()
 
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'llibreria.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
 )
