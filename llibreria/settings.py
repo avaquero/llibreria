c535429e09f19b0037222882075669e4d95c28f9
juli <juli.fer14@gmail.com>
2014-01-17 16:24:30 +0100

    Corregint errors

--- llibreria/settings.py
+++ llibreria/settings.py
@@ -61,8 +61,13 @@ WSGI_APPLICATION = 'llibreria.wsgi.application'
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     
-    'django.core.context_processors.request',
-    'django.contrib.auth.context_processors.auth,'
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages",
     #social auth
     'social.apps.django_app.context_processors.backends',
     'social.apps.django_app.context_processors.login_redirect',
