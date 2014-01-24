66f4faa4137bc0070523c0ddad8e329f266e59ea
juli <juli.fer14@gmail.com>
2014-01-24 16:39:57 +0100

    Edició de templates i noves vistes

--- llibres/views.py
+++ llibres/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from llibres.models import Titol, Genere, Llibre
 from django.http.response import HttpResponseRedirect
-from llibres.forms import FormGenere, FormLlibre
+from llibres.forms import FormGenere, FormLlibre, FormTitol
 from django.contrib import messages
 
 
@@ -19,6 +19,11 @@ def fitxaLlibre(request, idLlibre):
     llibre = get_object_or_404(Llibre, pk=idLlibre)
     context = {'llibre': llibre}
     return render(request, 'fitxaLlibre.html', context)
+
+def fitxaTitol(request, idTitol):
+    llibre = get_object_or_404(Llibre, pk=idTitol)
+    context = {'llibre': llibre}
+    return render(request, 'fitxaTitol.html', context)
 
 def entradaLlibre(request, idLlibre =  None):
     #Si idLlibre es None creem un nou llibre, sinó l'editem
@@ -69,11 +74,36 @@ def entradaGeneres(request, idGenere = None):
     return render(request, 'entradaGenere.html', {'form':form,})
 
 def llistatTitolsDeGenere(request, idGenere):
+    #TODO No acabat!
     titols = get_object_or_404(Titol, genere = idGenere )
     genere = get_object_or_404(Genere, pk=idGenere)
     context = {'titols':titols, 'genere':genere}
     
     return render(request, 'llistaTitolsGenere.html', context)
     
+def entradaTitols(request, idTitol = None):
+    #Si idGenere es None creem un nou genere, sinó editem
+    if idTitol is not None:
+        titol = get_object_or_404(Titol, pk=idTitol)
+    else:
+        titol = Titol()
+    #Si el metode es POST tractem les dades
+    if request.method == 'POST':
+        form = FormTitol(request.POST, instance = titol)
+        #Si les dades son correctes, les processem i redirigim a la llista de generes
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Titol introduit correctament')
+            return HttpResponseRedirect('/llibres/llistatTitols')
+        else:
+            messages.error(request, 'Ep! Hi ha hagut un error al introduir un titol')
+    #Si no es POST serà GET, mostrem el formulari buit
+    else:
+        form = FormTitol(instance=titol)
+        
+    return render(request, 'entradaTitol.html', {'form':form,})
 
-
+def llistatTitols(request):
+    titols = Titol.objects.all()
+    context = {'titols':titols}
+    return render(request, 'llistatTitol.html', context)
