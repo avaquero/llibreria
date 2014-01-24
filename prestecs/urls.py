ee0e2d7304ce988cdadb075fd0286dd4129c2798
avaquero <avaquero1469@gmail.com>
2014-01-24 16:13:27 +0100

    Prestecs modificats editable al fer clic al llistat de prestecs

--- prestecs/urls.py
+++ prestecs/urls.py
@@ -3,5 +3,6 @@ from prestecs import views
 
 urlpatterns = patterns('',
     url(r'^$', views.llistatPrestecs, name='llistatPrestecs'),
-    url(r'^nouPrestec/$', views.nouPrestec, name='nouPrestec'),   
+    url(r'^nouPrestec/$', views.nouPrestec, name='nouPrestec'),
+    url(r'^editarPrestec/(?P<idPrestec>\d+)$',views.nouPrestec, name='edita_prestec'), 
 )
--- prestecs/views.py
+++ prestecs/views.py
@@ -13,19 +13,23 @@ def llistatPrestecs(request):
     context = {'prestecs':prestecs}
     return render(request, 'prestecs.html', context)
 
-def nouPrestec(request):
+def nouPrestec(request, idPrestec =  None):
+    #Si idLlibre es None creem un nou llibre, sinó l'editem
+    if idPrestec is not None:
+        prestec = get_object_or_404(Prestec, pk = idPrestec)
+    else:
+        prestec = Prestec()
     #Si el metode es POST tractem les dades
     if request.method == 'POST':
-        form = FormPrestec(request.POST)
-        #Si les dades son correctes, les processem i redirigim a la llista de generes
+        form = FormPrestec(request.POST, instance = prestec)
+    #Si les dades son correctres, les procressem i redirigim a la llista de prestecs
         if form.is_valid():
             form.save()
             messages.success(request, 'Prestec introduit correctament')
             return HttpResponseRedirect('/prestecs')
         else:
-            messages.error(request, 'Ep! Hi ha hagut un error al introduir un prestec')
+            messages.error(request, "Ep! Hi ha hagut un error al introduir un prestec")
     #Si no es POST serà GET, mostrem el formulari buit
     else:
-        form = FormPrestec()
-        
+        form = FormPrestec(instance = prestec)
     return render(request, 'entrarPrestec.html', {'form':form,})
