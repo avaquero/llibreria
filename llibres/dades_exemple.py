677f92a4c52cac3437ea4eface9e18ce2cf06cf1
Julian <juli.fer14@gmail.com>
2014-01-26 23:24:19 +0100

    Dades exemple

--- llibres/dades_exemple.py
+++ llibres/dades_exemple.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 from usuaris.models import Perfil
 
 def crea():
+    #--------------------USUARIS---------------------------------
     pepe_usr = User.objects.create_user( 'pepe', 'pepe@bookshare.cat', 'pepe1' )
     jose_usr = User.objects.create_user( 'jose', 'jose@bookshare.cat', 'jose1' )
     pepa_usr = User.objects.create_user( 'pepa', 'pepa@bookshare.cat', 'pepa1' )
@@ -25,10 +26,14 @@ def crea():
     pepa = Perfil()
     pepa.usuari = pepa_usr
     pepa.save()
-    
+    #------------------------GENERES------------------------------------
     fantasia = Genere()
     fantasia.nom = "Fantasia"
     fantasia.save()
+    
+    aventura = Genere()
+    aventura.nom = "Aventura"
+    aventura.save()
 
     terror = Genere()
     terror.nom = "Terror"
@@ -37,13 +42,19 @@ def crea():
     romantico = Genere()
     romantico.nom = "Romantico"
     romantico.save()
-
+    #---------------------Titols---------------------------
     harrypotter = Titol()
     harrypotter.titol = "Harry Potter i la pedra filosofal"
     harrypotter.sinopsis = "Primer de Harry Potter"
     harrypotter.genere = fantasia
     harrypotter.save()
-
+    amor = Titol(titol = "Amor Amor", sinopsis = "Llibre d'amor", genere = romantico)
+    amor.save()
+    amor = Titol(titol = "Amor Amor", sinopsis = "Llibre d'amor", genere = romantico)
+    amor.save()
+    grito = Titol(titol ="Grito",sinopsis ="Terror el grito",genere =terror)
+    grito.save()
+    #-----------------------------Llibres-----------------
     harry1 = Llibre()
     harry1.idioma = "Català"
     harry1.isbn = "0000000000001"
@@ -53,14 +64,8 @@ def crea():
     harry1.propietari = pepe
     harry1.save()
     
-    amor = Titol(titol = "Amor Amor", sinopsis = "Llibre d'amor", genere = romantico)
-    amor.save()
-    
     amor1 = Llibre(idioma = "Català",isbn = "0000000000002",edicio = "Segona",editorial = "La Catalana", titol = amor, propietari = pepa)
     amor1.save()
-    
-    grito = Titol(titol ="Grito",sinopsis ="Terror el grito",genere =terror)
-    grito.save()
     
     grito1 = Llibre(idioma = "Català", isbn = "0000000000003", edicio = "Tercera", editorial = "Maya", titol = grito, propietari = jose)
     grito1.save()
