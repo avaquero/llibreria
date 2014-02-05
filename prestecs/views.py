eea83931115b9761a0e89746d844880af3adc778
avaquero <avaquero1469@gmail.com>
2014-02-05 18:57:20 +0100

    Solicituds prestecs

--- prestecs/views.py
+++ prestecs/views.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 from django.shortcuts import render, get_object_or_404
 from prestecs.models import Prestec, Solicitut_Prestec
-from prestecs.forms import FormPrestec
+from prestecs.forms import FormPrestec, FormSolicitutPrestec
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.http.response import HttpResponseRedirect
@@ -13,6 +13,12 @@ def llistatPrestecs(request):
     prestecs = Prestec.objects.all()
     context = {'prestecs':prestecs}
     return render(request, 'prestecs.html', context)
+
+@login_required
+def llistatSolicituds(request):
+    prestecs = Solicitut_Prestec.objects.all()
+    context = {'solicituds':solicituds}
+    return render(request, 'solicituds.html', context)
 
 @login_required
 def nouPrestec(request, idPrestec =  None):
@@ -35,3 +41,25 @@ def nouPrestec(request, idPrestec =  None):
     else:
         form = FormPrestec(instance = prestec)
     return render(request, 'entrarPrestec.html', {'form':form,})
+
+@login_required
+def solicitudPrestec(request, idSolicitud =  None):
+    #Si idLlibre es None creem un nou llibre, sinó l'editem
+    if idSolicitud is not None:
+        solicitud = get_object_or_404(Solicitut_Prestec, pk = idSolicitud)
+    else:
+        solicitud = Solicitut_Prestec()
+    #Si el metode es POST tractem les dades
+    if request.method == 'POST':
+        form = FormSolicitutPrestec(request.POST, instance = solicitud)
+    #Si les dades son correctres, les procressem i redirigim a la llista de prestecs
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Prestec introduit correctament')
+            return HttpResponseRedirect('/solicituds')
+        else:
+            messages.error(request, "Ep! Hi ha hagut un error al introduir un prestec")
+    #Si no es POST serà GET, mostrem el formulari buit
+    else:
+        form = FormSolicitutPrestec(instance = solicitud)
+    return render(request, 'solicitudPrestec.html', {'form':form,})
